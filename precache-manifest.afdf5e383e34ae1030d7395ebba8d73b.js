56c907dd91dd3ce5e376023a9af932b18e8ece78
Arturo Montoya <montoya332@live.com>
2019-08-09 17:40:20 -0700

    Updates

---
+++ precache-manifest.afdf5e383e34ae1030d7395ebba8d73b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d2cb9815cf1c1169d08a2e9cf46bb3",
+    "url": "/helping-families/index.html"
+  },
+  {
+    "revision": "2e819283d4b8b7cc1b67",
+    "url": "/helping-families/static/css/2.a5ead677.chunk.css"
+  },
+  {
+    "revision": "80e7819b2015597e67d7",
+    "url": "/helping-families/static/css/main.8f4f5982.chunk.css"
+  },
+  {
+    "revision": "2e819283d4b8b7cc1b67",
+    "url": "/helping-families/static/js/2.9e1b7e7d.chunk.js"
+  },
+  {
+    "revision": "80e7819b2015597e67d7",
+    "url": "/helping-families/static/js/main.c3f4719d.chunk.js"
+  },
+  {
+    "revision": "24e826ecbb69c85d4be7",
+    "url": "/helping-families/static/js/runtime~main.faa80e23.js"
+  },
+  {
+    "revision": "552d4670cf5c417cb83aa46a0d0a8569",
+    "url": "/helping-families/static/media/classRoom.552d4670.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/helping-families/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/helping-families/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/helping-families/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/helping-families/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/helping-families/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/helping-families/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/helping-families/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/helping-families/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/helping-families/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/helping-families/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/helping-families/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/helping-families/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/helping-families/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/helping-families/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/helping-families/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
