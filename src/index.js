9f77e27eda236c54409077448e89c36ea1e4c3d3
Steve Doty <stevedoty16@gmail.com>
2020-03-23 16:58:28 -0700

    add css files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
