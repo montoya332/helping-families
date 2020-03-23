d481fb1a22107ce934153a1244934e53be0bddc6
Steve Doty <stevedoty16@gmail.com>
2020-03-23 15:38:43 -0700

    added <Classroom /> component

---
+++ src/Components/Classroom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Classroom = () => {
+  return (
+    <div className="Classroom">
+        <div class="cta-content">
+            <div class="container">
+              <h2>Stop waiting.<br/>Start Getting Involved.</h2>
+              <a href="#contact" class="btn btn-outline btn-xl js-scroll-trigger">Let's Get Started!</a>
+            </div>
+          </div>
+          <div class="overlay"></div>
+    </div>
+  );
+}
+
+export default Classroom;
