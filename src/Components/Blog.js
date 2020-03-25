1caf6fa5782e63f7867a72af11ab8afa43855b4f
Steve Doty <stevedoty16@gmail.com>
2020-03-25 13:47:30 -0700

    added header to workshops

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Workshops = [{
+    header: 'Workshop',
     title: 'Helpful Tips During Social Distancing',
     date: 'March 24 6:30 PM PDT',
     summary: 'Helping Families in Silicon Valley is hosting a Q/A webinar during social distancing with guest speaker RN Lorena Torres.',
@@ -8,12 +9,13 @@ const Workshops = [{
     imgUrl: '',
 },
 {
+    header: 'Workshop',
     title: 'SMART GOALS',
     date: 'March 11',
     summary: 'This workshop provides an overview of a simple but effective model ...',
     url: 'https://www.eventbrite.com/e/smart-goals-setting-with-adrienne-lawson-tickets-95119604297?ref=eios',
     imgUrl: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F92912059%2F408932228207%2F1%2Foriginal.20200218-001752?w=1080&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C890%2C445&s=bea7c2ef362449fb8ef6305721af5e80',
-},]
+}]
 const Blog = () => {
   return (
         <section id="blog" class="pb-5 bg-light page-section" >
@@ -26,7 +28,7 @@ const Blog = () => {
                                 <div className="col-md-6">
                                   <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                                     <div className="col p-4 d-flex flex-column position-static">
-                                      <strong className="d-inline-block mb-2 text-primary">Workshop</strong>
+                                      <strong className="d-inline-block mb-2 text-primary">{workshop.header}</strong>
                                       <h3 className="mb-0">{workshop.title}</h3>
                                       <div className="mb-1 text-muted">{workshop.date}</div>
                                       <p className="card-text mb-auto">
