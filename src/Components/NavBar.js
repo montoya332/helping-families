2c86c27248f752c16a521dc3d42f0f29ed34a05a
Steve Doty <stevedoty16@gmail.com>
2020-03-25 14:03:32 -0700

    add prettier

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,21 +3,25 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-none d-sm-inline mr-2"><img src="logo.svg" alt="" height="40" /></span>
-                Helping Families in Silicon Valley
-            </a>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          <span className="d-none d-sm-inline mr-2">
+            <img src="logo.svg" alt="" height="40" />
+          </span>
+          Helping Families in Silicon Valley
+        </a>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
--- src/Components/Vision.js
+++ src/Components/Vision.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 const Vision = () => {
   return (
-        <section className="download bg-navy-blue-3 text-white text-center" id="download">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 mx-auto">
-                        <h2 className="section-heading">
-                            Our vision is to uplift and strengthen parents, guardians, and individuals to help them reach their full potential.
-                        </h2>
-                        <p>Contact Us to get started!</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+    <section className="download bg-navy-blue-3 text-white text-center" id="download">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto">
+            <h2 className="section-heading">
+              Our vision is to uplift and strengthen parents, guardians, and individuals to help them reach their full
+              potential.
+            </h2>
+            <p>Contact Us to get started!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Vision;
