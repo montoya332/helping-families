84695ee65eb60fa8e77f97f6d2c3853e82cfa175
Steve Doty <stevedoty16@gmail.com>
2020-03-28 10:51:52 -0700

    refactor NavBar with react-bootstrap

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <Navbar fixed="top" id="mainNav">
       <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+        <Navbar.Brand className="navbar-brand js-scroll-trigger" href="#page-top">
           <span className="d-none d-sm-inline mr-2">
             <img src="logo.svg" alt="" height="40" />
           </span>
           Helping Families in Silicon Valley
-        </a>
+        </Navbar.Brand>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
@@ -20,7 +21,7 @@ const NavBar = () => {
           </ul>
         </div>
       </div>
-    </nav>
+    </Navbar>
   );
 };
 
