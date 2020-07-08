1985a169244b46fc5899d698df08335f9c13ac79
Arturo Montoya <montoya332@live.com>
2020-07-07 18:05:50 -0700

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = (props) => {
   return (
     <Navbar expanded="lg" fixed="top" variant="light" id="mainNav">
-      <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">
-          <span className="d-none d-sm-inline">
-            <img src="logo.svg" alt="" height="40" className="mr-2" />
-          </span>
+      <Navbar.Brand href="#home">
+        <span className="d-none d-lg-inline">
+          <img src="logo.svg" alt="" height="40" className="mr-2"  />
           {props.company.companyName}
-        </a>
-        <button className="navbar-toggler collapsed" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <button
-          className="navbar-toggler collapsed"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarsExample07XL"
-          aria-controls="navbarsExample07XL"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+        </span>
+        <span className="d-lg-none d-xl-none">
+          <img src="logo_dark.svg" alt="" height="40" className="mr-2"  />
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
