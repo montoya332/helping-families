721c10ae4300b67112e929a30ac1205e66765743
Arturo Montoya <montoya332@live.com>
2020-07-07 18:10:05 -0700

    fix top navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = (props) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" fixed="top">
       <Navbar.Brand href="#home">
         <span className="d-none d-lg-inline">
           <img src="logo.svg" alt="" height="40" className="mr-2" />
