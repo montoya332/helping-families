ac58b4794b7dbf89a69f261095a813130429ad93
Arturo Montoya <montoya332@live.com>
2020-07-07 18:08:37 -0700

    Nav Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = (props) => {
   return (
-    <Navbar expanded="lg" fixed="top" variant="light" id="mainNav">
+    <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#home">
         <span className="d-none d-lg-inline">
-          <img src="logo.svg" alt="" height="40" className="mr-2"  />
+          <img src="logo.svg" alt="" height="40" className="mr-2" />
           {props.company.companyName}
         </span>
         <span className="d-lg-none d-xl-none">
-          <img src="logo_dark.svg" alt="" height="40" className="mr-2"  />
+          <img src="logo_dark.svg" alt="" height="40" className="mr-2" />
         </span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
