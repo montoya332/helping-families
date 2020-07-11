0a8310cbe4250bbb5ca6668ccf49ec03070f9c21
Arturo Montoya <montoya332@live.com>
2020-07-11 16:02:45 -0700

    remove jquery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
